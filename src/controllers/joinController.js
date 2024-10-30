ab8f3f388feefad9b3aab48fd54883a78c80ad0b
koinonia2 <koinonia_ys@naver.com>
2024-10-30 15:04:15 +0900

    [add] 로그인, 비밀번호 해시

--- src/controllers/joinController.js
+++ src/controllers/joinController.js
@@ -1,7 +1,8 @@
 import User from "../model/user";
+import bcrypt from 'bcryptjs';
 
 export const idCheck = async (req, res) => {
-    console.log(req.body);
+    // console.log(req.body);
     const { id } = req.body;
 
     try {
@@ -19,17 +20,43 @@ export const idCheck = async (req, res) => {
 
 export const join = async (req, res) => {
     try{
-        console.log(req.body);
+        // console.log(req.body);
         const {id, password, name, phone, email} = req.body;
+        // 비밀번호 해시
+        const hashedPassword = await bcrypt.hash(password, 10);
+
         const data = await User.create({
             id,
-            password,
+            password : hashedPassword,
             name,
             phone,
             email,
             createdAt: new Date(Date.now() + 9 * 60 * 60 * 1000)
         });
         return res.send({result: true, data});
+    }catch(error){
+        console.log(error);
+        return res.send({result: false});
+    };
+};
+
+export const login = async (req, res) => {
+    try{
+        // console.log(req.body);
+        const { id, password } = req.body;
+
+        const user = await User.findOne({ id });
+        if (!user) {
+            return res.status(401).send({ result: false, message: "아이디 또는 비밀번호가 잘못되었습니다." });
+        }
+        // 입력한 비밀번호와 저장된 해시된 비밀번호 비교
+        const isMatch = bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return res.status(401).send({ result: false, message: "아이디 또는 비밀번호가 잘못되었습니다." });
+        };
+
+        // 로그인 성공
+        return res.status(200).send({ result: true, data: { id: user.id, name: user.name } });
     }catch(error){
         console.log(error);
         return res.send({result: false});
--- src/router/joinrouter.js
+++ src/router/joinrouter.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { idCheck, join } from "../controllers/joinController";
+import { idCheck, join, login } from "../controllers/joinController";
 
 const joinRouter = express.Router();
 
 joinRouter.post("", join);
 joinRouter.post("/idCheck", idCheck);
+joinRouter.post("/login", login);
 
 export default joinRouter;
