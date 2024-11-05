48271a779c6fed09fb2a951cf68425b8abb58545
koinonia2 <koinonia_ys@naver.com>
2024-11-05 17:09:42 +0900

    [add] delete

--- src/controllers/memoController.js
+++ src/controllers/memoController.js
@@ -15,7 +15,7 @@ export const memoWrite = async (req, res) => {
             reference,
             createdAt: new Date(Date.now() + 9 * 60 * 60 * 1000)
         });
-        return res.status(201).json({ result: true, message: "메모 저장 완료" });
+        return res.status(201).json({ result: true, message: "메모 저장 완료", data });
     } catch (error) {
         console.error(error);
         return res.status(500).json({ result: false, message: "죄송합니다. 서버에 문제가 발생했습니다. 잠시 후 다시 시도해 주세요." });
@@ -31,6 +31,24 @@ export const memoList = async (req, res) => {
             return res.status(200).json({ result: false, message: "등록된 메모가 없습니다.", memos: [] });
         };
         return res.status(200).json({ result: true, memos });
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ result: false, message: "죄송합니다. 서버에 문제가 발생했습니다. 잠시 후 다시 시도해 주세요." });
+    }
+};
+
+export const memoDelete = async (req, res) => {
+    const { ids } = req.body; // 삭제할 메모의 ID 목록
+
+
+    if (!ids || !Array.isArray(ids)) {
+        return res.status(400).json({ result: false, message: "삭제할 메모를 선택해주세요." });
+    }
+
+    try {
+        // ids 배열에 있는 _id에 해당하는 메모들을 삭제
+        await Memo.deleteMany({ _id: { $in: ids } });
+        return res.status(200).json({ result: true, message: "선택한 메모가 삭제되었습니다." });
     } catch (error) {
         console.error(error);
         return res.status(500).json({ result: false, message: "죄송합니다. 서버에 문제가 발생했습니다. 잠시 후 다시 시도해 주세요." });
--- src/router/memoRouter.js
+++ src/router/memoRouter.js
@@ -1,10 +1,12 @@
 import express from "express";
-import { memoList, memoWrite } from "../controllers/memoController";
+import { memoDelete, memoList, memoWrite } from "../controllers/memoController";
 import { verifyToken } from "../middlewears/verifyToken";
 
 const memoRouter = express.Router();
 
 memoRouter.post("/write", verifyToken, memoWrite);
 memoRouter.get("/list", verifyToken, memoList);
+memoRouter.post("/delete", verifyToken, memoDelete);
+
 
 export default memoRouter;
